af789ada071f400525f90924221b266ccb2c198c
marisa <marisa.mayumi.mo@gmail.com>
2025-02-21 15:41:15 -0300

    terminada a 1 aula

--- src/components/icon-button.tsx
+++ src/components/icon-button.tsx
@@ -6,7 +6,7 @@ interface IconButtonProps extends ComponentProps<'button'>{}
 
 export function IconButton (props : IconButtonProps) {
   return (
-    <button 
+    <button
         className="p-1.5 bg-gray-500 text-blue rounded-md cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-900"
         {...props}
     />
